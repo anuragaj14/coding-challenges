92739d67361280e16256bcd06ad15bedcc1d6097
Anurag Janbandhu <35415174+anuragaj14@users.noreply.github.com>
2022-05-16 22:14:48 +0530

    Create LongestIncreasingSubsequence.java
    https://workat.tech/problem-solving/practice/longest-increasing-subsequence/editorial

---
+++ LongestIncreasingSubsequence.java
@@ -0,0 +1,24 @@
+//https://workat.tech/problem-solving/practice/longest-increasing-subsequence/editorial
+class LongestIncreasingSubsequence {
+	int getLengthOfLIS(int[] A) {
+    	// add your logic here
+		int [] max = new int[A.length];
+		for(int i = 0 ; i<max.length; i++) {
+			max[i]=1;
+		}
+		for(int i= 0;i<A.length;i++){
+			for(int j=0;j<i;j++){
+				if(A[i]>A[j] && max[j]>=max[i]) {
+					max[i]=1+max[j];
+				}
+			}
+		}
+		int lis =0;
+		for(int i = 0 ; i<max.length; i++) {
+			if(max[i]>lis){
+				lis = max[i];
+			}
+		}
+		return lis;
+	}
+}
